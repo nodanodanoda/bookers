0b6fd56e17aae95ac7bff1b183fe88c378545328
nodanodanoda <61902364+nodanodanoda@users.noreply.github.com>
2020-04-20 20:42:42 +0900

    [finish]4-8

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,56 @@
+class BooksController < ApplicationController
+  def top
+  end
+
+  def index
+  	@book = Book.new
+  	@index = Book.all
+  end
+
+   def create
+    
+     book = Book.new(book_params)
+    
+     if book.save
+       redirect_to book_path(book.id)
+     else
+      @book = book
+      @index = Book.all
+      render :index
+      end
+   end
+
+  def show
+  	@book = Book.find(params[:id])
+  end
+
+  def edit
+  	@book = Book.find(params[:id])
+  end
+
+  def update
+      @book = Book.find(params[:id])
+      
+      if @book.update(book_params)
+         redirect_to book_path(@book.id), notice: 'Book was successfully updated.'
+
+       else
+
+        render :edit
+
+      end
+
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path(@book.id), notice: 'Book was successfully destroyed.'
+  end
+
+  private
+   
+    def book_params
+        params.require(:book).permit(:title, :body)
+    end
+end
